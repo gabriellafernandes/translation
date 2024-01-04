27be6c72e8631bfd1184e2eaefa31056170e45ee
Gabriella Fernandes <93540291+gabriellafernandes@users.noreply.github.com>
2024-01-04 21:23:45 +0000

    noisy text panda

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import math
-
+import pandas as pd
 
 def snr_to_noise_level(snr_dB):
     noise_l = math.sqrt(1 / (10 ** (snr_dB / 10)))
@@ -35,20 +36,20 @@ def add_awgn_noise(sentence, noise_l):
     return noisy_sentence
     
     
-
-# Example usage
 snr_dB = 0
 noise_level = snr_to_noise_level(snr_dB)
 
-# Example usage
-original_sentence = "This is a sample sentence."
-noisy_sentence = add_awgn_noise(original_sentence, noise_level)
+df = pd.read_fwf("europarl-v7.de-en.txt")
+df = df.dropna(axis=1)
+print('a')
 
-print("Original Sentence:", original_sentence)
-print("Noisy Sentence:", noisy_sentence)
+noise_translation = []
+for i in df['Resumption of the session']:
+    noise_translation.append(add_awgn_noise(i, noise_level))
+    
+df['Noisy'] = noise_translation
 
+print(df)
 
-
-
-print(f"SNR: {snr_dB} dB")
-print(f"Noise Level: {noise_level}")
+print('a')
+df.to_csv('output.csv', index=False)
